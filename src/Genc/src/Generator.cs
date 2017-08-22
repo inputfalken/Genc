bd6d8517f9417b2ebf67c6ee4a17f4bde0bb4f06
Robert Andersson <inputfalken@gmail.com>
2017-08-22 03:46:43 -0700

    Cleanup code

--- src/Genc/src/Generator.cs
+++ src/Genc/src/Generator.cs
@@ -8,7 +8,7 @@ namespace Genc {
     public static class Generator {
         /// <summary>
         ///     <para>
-        ///         Creates an infinite <see cref="IObservable{T}" /> whose elements are of the type <see cref="int"/> that are
+        ///         Creates an infinite <see cref="IObservable{T}" /> whose elements are of the type <see cref="int" /> that are
         ///         greater than or equal to argument <paramref name="min" /> and less than argument <paramref name="max" />.
         ///     </para>
         /// </summary>
@@ -23,26 +23,25 @@ namespace Genc {
         ///     number is specified, the absolute value of the number is used.
         /// </param>
         /// <returns>
-        ///   An <see cref="IObservable{T}" /> randomizing integers.
+        ///     An <see cref="IObservable{T}" /> randomizing integers.
         /// </returns>
         /// <example>
         ///     <code language="C#" region="RandomizerThreeArg" source="Examples\GeneratorFactory.cs" />
         /// </example>
         public static IObservable<int> Randomizer(int min, int max, int? seed = null) {
-            if (max > min) {
-                return Observable.Generate(
+            return max > min
+                ? Observable.Generate(
                     CreateRandom(seed),
                     random => true,
                     random => random,
                     random => random.Next(min, max)
-                );
-            }
-            throw new ArgumentOutOfRangeException(nameof(max), @"max must be > min!");
+                )
+                : throw new ArgumentOutOfRangeException(nameof(max), @"max must be > min!");
         }
 
         /// <summary>
         ///     <para>
-        ///         Creates an infinite <see cref="IObservable{T}" /> whose elements are of the type <see cref="long"/> that are
+        ///         Creates an infinite <see cref="IObservable{T}" /> whose elements are of the type <see cref="long" /> that are
         ///         greater than or equal to argument <paramref name="min" /> and less than argument <paramref name="max" />.
         ///     </para>
         /// </summary>
@@ -57,32 +56,31 @@ namespace Genc {
         ///     number is specified, the absolute value of the number is used.
         /// </param>
         /// <returns>
-        ///   An <see cref="IObservable{T}" /> randomizing longs.
+        ///     An <see cref="IObservable{T}" /> randomizing longs.
         /// </returns>
         public static IObservable<long> Randomizer(long min, long max, int? seed = null) {
-            if (max <= min)
-                throw new ArgumentOutOfRangeException(nameof(max), @"max must be > min!");
-            //Working with ulong so that modulo works correctly with values > long.MaxValue
-            var uRange = (ulong) (max - min);
-            return Observable.Generate(
-                CreateRandom(seed),
-                rnd => true,
-                rnd => rnd,
-                rnd => {
-                    //Prevent a modulo bias; see http://stackoverflow.com/a/10984975/238419
-                    //for more information.
-                    //In the worst case, the expected number of calls is 2 (though usually it's
-                    //much closer to 1) so this loop doesn't really hurt performance at all.
-                    ulong ulongRand;
-                    do {
-                        var buf = new byte[8];
-                        rnd.NextBytes(buf);
-                        ulongRand = (ulong) BitConverter.ToInt64(buf, 0);
-                    } while (ulongRand > ulong.MaxValue - (ulong.MaxValue % uRange + 1) % uRange);
+            return max > min
+                ? Observable.Generate(
+                    new Tuple<Random, ulong>(CreateRandom(seed), (ulong) (max - min)),
+                    tuple => true,
+                    tuple => tuple,
+                    tuple => {
+                        //Prevent a modulo bias; see http://stackoverflow.com/a/10984975/238419
+                        //for more information.
+                        //In the worst case, the expected number of calls is 2 (though usually it's
+                        //much closer to 1) so this loop doesn't really hurt performance at all.
+                        ulong ulongRand;
+                        do {
+                            var buf = new byte[8];
+                            tuple.Item1.NextBytes(buf);
+                            ulongRand = (ulong) BitConverter.ToInt64(buf, 0);
+                        } while (ulongRand > ulong.MaxValue - (ulong.MaxValue % tuple.Item2 + 1) % tuple.Item2);
 
-                    return (long) (ulongRand % uRange) + min;
-                }
-            );
+                        return (long) (ulongRand % tuple.Item2) + min;
+                    }
+                )
+                : throw new ArgumentOutOfRangeException(nameof(max), @"max must be > min!");
+            //Working with ulong so that modulo works correctly with values > long.MaxValue
         }
 
 
@@ -115,8 +113,8 @@ namespace Genc {
 
         /// <summary>
         ///     <para>
-        ///         Creates an finite <see cref="IObservable{T}" /> whose elements are of the type <see cref="int"/> 
-        ///         which get incremented by 1 until it reaches the value of <see cref="int.MaxValue"/>.
+        ///         Creates an finite <see cref="IObservable{T}" /> whose elements are of the type <see cref="int" />
+        ///         which get incremented by 1 until it reaches the value of <see cref="int.MaxValue" />.
         ///     </para>
         /// </summary>
         /// <param name="start">The inclusive number to start at.</param>
@@ -134,8 +132,8 @@ namespace Genc {
 
         /// <summary>
         ///     <para>
-        ///         Creates an finite <see cref="IObservable{T}" /> whose elements are of the type <see cref="long"/> 
-        ///         which get incremented by 1 until it reaches the value of <see cref="long.MaxValue"/>.
+        ///         Creates an finite <see cref="IObservable{T}" /> whose elements are of the type <see cref="long" />
+        ///         which get incremented by 1 until it reaches the value of <see cref="long.MaxValue" />.
         ///     </para>
         /// </summary>
         /// <param name="start">The inclusive number to start at.</param>
@@ -145,16 +143,16 @@ namespace Genc {
         public static IObservable<long> Incrementer(long start) {
             return Observable.Generate(
                 start - 1,
-                i => i < long.MaxValue,
-                i => i + 1,
-                i => i + 1
+                l => l < long.MaxValue,
+                l => l + 1,
+                l => l + 1
             );
         }
 
         /// <summary>
         ///     <para>
-        ///         Creates an finite <see cref="IObservable{T}" /> whose elements are of the type <see cref="int"/> 
-        ///         which get decremented by 1 until it reaches the value of <see cref="int.MinValue"/>.
+        ///         Creates an finite <see cref="IObservable{T}" /> whose elements are of the type <see cref="int" />
+        ///         which get decremented by 1 until it reaches the value of <see cref="int.MinValue" />.
         ///     </para>
         /// </summary>
         /// <param name="start">The inclusive number to start at.</param>
@@ -165,20 +163,18 @@ namespace Genc {
         ///     <code language="C#" region="Decrementer" source="Examples\GeneratorFactory.cs" />
         /// </example>
         public static IObservable<int> Decrementer(int start) {
-            return start.Equals(int.MinValue)
-                ? throw new OverflowException($"Argument {nameof(start)} cannot be {int.MaxValue}.")
-                : Observable.Generate(
-                    start + 1,
-                    i => i > int.MinValue,
-                    i => i - 1,
-                    i => i - 1
-                );
+            return Observable.Generate(
+                start + 1,
+                i => i > int.MinValue,
+                i => i - 1,
+                i => i - 1
+            );
         }
 
         /// <summary>
         ///     <para>
-        ///         Creates an finite <see cref="IObservable{T}" /> whose elements are of the type <see cref="long"/> 
-        ///         which get decremented by 1 until it reaches the value of <see cref="long.MinValue"/>.
+        ///         Creates an finite <see cref="IObservable{T}" /> whose elements are of the type <see cref="long" />
+        ///         which get decremented by 1 until it reaches the value of <see cref="long.MinValue" />.
         ///     </para>
         /// </summary>
         /// <param name="start">The inclusive number to start at.</param>
@@ -186,14 +182,12 @@ namespace Genc {
         ///     An <see cref="IObservable{T}" /> with <see cref="long" /> as its generic type.
         /// </returns>
         public static IObservable<long> Decrementer(long start) {
-            return start.Equals(int.MinValue)
-                ? throw new OverflowException($"Argument {nameof(start)} cannot be {long.MaxValue}.")
-                : Observable.Generate(
-                    start + 1,
-                    i => i > long.MinValue,
-                    i => i - 1,
-                    i => i - 1
-                );
+            return Observable.Generate(
+                start + 1,
+                l => l > long.MinValue,
+                l => l - 1,
+                l => l - 1
+            );
         }
     }
 }
